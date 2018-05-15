77b32bb3d702b93fe9d37cdc07748dc9db20edae
Alex Wilson <fonzmp@gmail.com>
2018-05-15 21:07:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,8 @@ def key_for_min_value(name_hash)
   else
     lowest = ""
     name_hash.each do |key, value|
-      puts "#{key}: #{value}"
+      if value < lowest
+        lowest = value
     end
   end
 end
